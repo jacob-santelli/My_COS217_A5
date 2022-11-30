1cad543cebda895d2bf045117b5d34a3d04b5fb7
joshuaqyang <joshuayang2014@gmail.com>
2022-11-30 16:56:08 -0500

    more stuff

--- mywc.s
+++ mywc.s
@@ -2,12 +2,25 @@
    .equ TRUE, 1
 
    .section .data
+// static long lLineCount = 0;
 lLineCount: .quad 0
+// static long lWordCount = 0;
 lWordCount: .quad 0
+// static long lCharCount = 0;
 lCharCount: .quad 0
+// static int iInWord = FALSE;
 iINWord: .word FALSE
 
    .section .bss
-iChar: .word
+// static int iChar;
+iChar: .skip 4
 
-bl getchar
+   .global main
+main:
+loop1:
+   // if ((iChar = getchar()) == EOF) goto endloop1;
+   bl getchar
+   adr x0, iChar
+   str w0, [x0]
+
+

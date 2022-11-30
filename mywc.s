3fca24c66edecafa46537aea103f2a0398367666
joshuaqyang <joshuayang2014@gmail.com>
2022-11-30 15:12:24 -0500

    stuff

--- mywc.s
+++ mywc.s
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-.equ FALSE, 0
-.equ TRUE, 1
-=======
-   .equ FALSE, 0
-   .equ TRUE, 1
-
->>>>>>> 2c3fe3660427b0d997f898aceec4db976a1c2352
+        .equ FALSE, 0
+        .equ TRUE, 1
    .section .data
 lLineCount: .quad 0
 lWordCount: .quad 0
 lCharCount: .quad 0
-iINWord: .word FALSE
-
-   .section .bss
-iChar: .word
-
+iINWord: .word 1

00b7ce78cf249f1721642ec7a01c8f91a8f65eb8
santelli <santelli@princeton.edu>
2022-11-30 15:14:55 -0500

    b

--- mywc.s
+++ mywc.s
@@ -10,10 +10,4 @@ iINWord: .word FALSE
    .section .bss
 iChar: .word
 
-        .equ FALSE, 0
-        .equ TRUE, 1
-   .section .data
-lLineCount: .quad 0
-lWordCount: .quad 0
-lCharCount: .quad 0
-iINWord: .word 1
+bl getchar

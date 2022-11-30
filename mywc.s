2c3fe3660427b0d997f898aceec4db976a1c2352
santelli <santelli@princeton.edu>
2022-11-30 15:08:47 -0500

    b

--- mywc.s
+++ mywc.s
@@ -1,7 +1,12 @@
-        .equ FALSE, 0
-        .equ TRUE, 1
+   .equ FALSE, 0
+   .equ TRUE, 1
+
    .section .data
 lLineCount: .quad 0
 lWordCount: .quad 0
 lCharCount: .quad 0
-iINWord: .word 1
+iINWord: .word FALSE
+
+   .section .bss
+iChar: .word
+

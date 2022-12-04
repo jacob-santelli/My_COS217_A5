ce8230458803cbdf56f26e31ac13a862d9b2f6ea
joshuaqyang <joshuayang2014@gmail.com>
2022-12-04 16:25:11 -0500

    stuff

--- bigintadd.s
+++ bigintadd.s
@@ -6,6 +6,31 @@
 
    .section .text
 
+   // Must be a multiple of 16
+    .equ    LARGER_STACK_BYTECOUNT, 32
+        
+    // Local variable stack offsets:
+    .equ LLARGER, 8
+
+    // Parameter stack offsets:
+    .equ LLENGTH1, 16
+    .equ LLENGTH2, 24
+
+BigInt_larger:
+
+    // Prolog
+    sub sp, sp, LARGER_STACK_BYTECOUNT
+    str x30, [sp]
+    str x0, [sp, LLENGTH1]
+    str x1, [sp, LLENGTH2]
+
+    // long lLarger
+    
+
+
+
+
+
    .equ    MAIN_STACK_BYTECOUNT, 16
 
    // enum {FALSE, TRUE};
@@ -20,6 +45,7 @@ main:
 // prolog
 sub sp, sp, MAIN_STACK_BYTECOUNT
 str x30, [sp]
+
 
 
 // epilog

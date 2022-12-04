0c307f11998c6dbda1c3345653d6947a9b98319f
joshuaqyang <joshuayang2014@gmail.com>
2022-12-04 17:28:00 -0500

    stuff

--- temp.s
+++ temp.s
@@ -1,35 +1,20 @@
-   // if (ulCarry != 1) goto endif4;
-   str x0, [sp, ULCARRY]
-   cmp x0, 1
-   bne endif4
+   // if (ulSum >= oAddend2->aulDigits[lIndex]) goto endif3; /* Check for overflow. */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+         ulCarry = 1;
+      endif3:
 
-   // if (lSumLength != MAX_DIGITS) goto endif5;
-   str x0, [sp, LSUMLENGTH]
-   str x1, MAX_DIGITS
-   cmp x0, x1
-   bne endif5
-
-   // epilog, return FALSE;
-   mov x0, FALSE
-   ldr x30, [sp]
-   add sp, sp, ADD_STACK_BYTECOUNT
-   ret 
-
-endif5:
-   // oSum->aulDigits[lSumLength] = 1;
-   ldr x0, [sp, OSUM]
-   
-
-
-
-
-
-
-
-      lSumLength++;
-   endif4:
-
-   /* Set the length of the sum. */
-   oSum->lLength = lSumLength;
-
-   return TRUE;
+      oSum->aulDigits[lIndex] = ulSum;
+      lIndex++;
+      goto startfor1;
+   endfor1:

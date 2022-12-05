d9c1aae53c9c17605bf3564946e77c5538c928da
joshuaqyang <joshuayang2014@gmail.com>
2022-12-05 15:38:36 -0500

    stuff

--- bigintadd.s
+++ bigintadd.s
@@ -84,7 +84,6 @@ BigInt_add:
    str x1, [sp, OADDEND2]
    str x2, [sp, OSUM] 
 
-
    // unsigned long ulCarry;
    // unsigned long ulSum;
    // long lIndex;
@@ -227,6 +226,7 @@ endfor1:
    ldr x30, [sp]
    add sp, sp, ADD_STACK_BYTECOUNT
    ret 
+   .size   BigInt_add, (. - BigInt_add)
 
 endif5:
    // oSum->aulDigits[lSumLength] = 1;
@@ -251,3 +251,4 @@ endif4:
    ldr x30, [sp]
    add sp, sp, ADD_STACK_BYTECOUNT
    ret
+   .size   BigInt_add, (. - BigInt_add)
